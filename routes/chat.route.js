547bea551eab5e7b92e811b41a33b915f98849bd
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2023-09-30 23:19:54 +0100

    Bug-Fix: Changed default GET endpoint to POST

--- routes/chat.route.js
+++ routes/chat.route.js
@@ -2,6 +2,6 @@ import express from "express";
 import { ChatController } from "./../controllers/index.js";
 const chatRouter = express.Router();
 
-chatRouter.get("/", ChatController.replyChat);
+chatRouter.post("/", ChatController.replyChat);
 
 export { chatRouter };
